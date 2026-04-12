9cf057f169c26aeec17a859e5e173643400ba97b
zeet <3105327090@qq.com>
2026-04-12 11:35:41 +0800

    complete sys_get_time

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -11,11 +11,14 @@ pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     trace!("kernel:pid[{}] sys_write", current_task().unwrap().pid.0);
     match fd {
         FD_STDOUT => {
-            let buffers = translated_byte_buffer(current_user_token(), buf, len);
-            for buffer in buffers {
-                print!("{}", core::str::from_utf8(buffer).unwrap());
+            if let Some(buffers) = translated_byte_buffer(current_user_token(), buf, len, false) {
+                for buffer in buffers {
+                    print!("{}", core::str::from_utf8(buffer).unwrap());
+                }
+                len as isize
+            } else {
+                -1
             }
-            len as isize
         }
         _ => {
             panic!("Unsupported fd in sys_write!");
@@ -39,11 +42,15 @@ pub fn sys_read(fd: usize, buf: *const u8, len: usize) -> isize {
                 }
             }
             let ch = c as u8;
-            let mut buffers = translated_byte_buffer(current_user_token(), buf, len);
-            unsafe {
-                buffers[0].as_mut_ptr().write_volatile(ch);
+            if let Some(mut buffers) = translated_byte_buffer(current_user_token(), buf, len, true)
+            {
+                unsafe {
+                    buffers[0].as_mut_ptr().write_volatile(ch);
+                }
+                1
+            } else {
+                -1
             }
-            1
         }
         _ => {
             panic!("Unsupported fd in sys_read!");
--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -3,11 +3,12 @@ use alloc::sync::Arc;
 
 use crate::{
     loader::get_app_data_by_name,
-    mm::{translated_refmut, translated_str},
+    mm::{copy_to_user, translated_refmut, translated_str},
     task::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next,
     },
+    timer::get_time_us,
 };
 
 #[repr(C)]
@@ -67,7 +68,11 @@ pub fn sys_exec(path: *const u8) -> isize {
 /// If there is not a child process whose pid is same as given, return -1.
 /// Else if there is a child process but it is still running, return -2.
 pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
-    trace!("kernel::pid[{}] sys_waitpid [{}]", current_task().unwrap().pid.0, pid);
+    trace!(
+        "kernel::pid[{}] sys_waitpid [{}]",
+        current_task().unwrap().pid.0,
+        pid
+    );
     let task = current_task().unwrap();
     // find a child process
 
@@ -106,11 +111,21 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+    trace!("kernel: sys_get_time");
+
+    let us = get_time_us();
+    let timeval = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+
+    let result = copy_to_user(current_user_token(), _ts, &timeval);
+
+    if result {
+        0
+    } else {
+        -1
+    }
 }
 
 /// YOUR JOB: Implement mmap.
